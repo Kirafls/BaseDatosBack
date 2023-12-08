0f04715764fd296a021e0a7874976d0f7e3611cd
Saul Torres <carloscogake@gmail.com>
2023-12-07 20:20:11 -0600

    CAmbios iniciales

--- consultas.js
+++ consultas.js
@@ -1,6 +1,14 @@
 const mysql =require("mysql");
+//Codigos anteriores base para la creacion del nuevo codigo
+function login(conection,data,callback){
+    let query="SELECT 'contraseña' WHERE Id=? "
+}
+function registro(conection,data,callback){
+    let insertQuery="INSERT INTO `cuenta`(`id_Cuenta`, `id_Estudiante`, `saldo`, `contraseña`, `fecha_creacion`) VALUES (?,?,?,?,?)";
 
-function nuevo(conection,data,callback){
+}
+
+/*function nuevo(conection,data,callback){
     let insertQuery="INSERT INTO `user`(`Nombre`, `Apellido`, `Prepa`, `Carrera`, `Fecha_na`, `Genero`, `Promedio`) VALUES (?,?,?,?,?,?,?)";
     let query =mysql.format(insertQuery, [data.nombre,data.apellido,data.prepa,data.carrera,data.fecha,data.genero,data.promedio]);
     conection.query(query,function(err,result){
@@ -23,5 +31,6 @@ function eliminar(conection,data,callback){
         if(err) throw err;
         callback(result);
     });
-}
-module.exports={nuevo,mostrar,eliminar};
+}*/
+
+module.exports={login,registro};
--- index.js
+++ index.js
@@ -2,7 +2,11 @@ const express = require("express");
 const app = express();
 const puerto=3000;
 const mysql=require("mysql");
-const {nuevo, mostrar, eliminar}=require("./consultas");
+
+//Requiere actualizacion con el nuevo codigo
+//const {nuevo, mostrar, eliminar}=require("./consultas");
+
+
 const cors=require('cors');
 const bodyparse=require("body-parser");
 
@@ -15,12 +19,12 @@ const connection =mysql.createConnection({
     host:"localhost",
     user:"root",
     password:"",
-    database:"userdb",
+    database:"dbbeca",
 });
 
 connection.connect((err)=>{
     if(err) throw err;
-    console.log("Coneccion con la base de datos");
+    console.log("Conexion exitosa con DB ...");
 })
 
 //app.use(require("./routes/coreoRuta"));
@@ -30,7 +34,22 @@ app.get("/",(req,res)=>{
     res.send("Servidor corriendo");
 })
 
-app.post("/nuevo",(req,res)=>{
+app.post("/registro",(req,res)=>{
+    let id_Estudiante=req.body.id_Estudiante;
+    let saldo=0;
+    let contraseña=req.body.contraseña;
+    let fecha=new Date();
+})
+app.post("/login",(req,res)=>{
+    let id_Cuenta=req.body.id_Cuenta;
+    let contraseña=req.body.contraseña;
+    
+    nuevo(connection,{id_Cuenta:id_Cuenta},result=>{
+        res.send(result);
+    })
+})
+//Requiere actualizacion con el nuevo codigo 
+/*app.post("/nuevo",(req,res)=>{
     let nombre=req.body.nombre;
     let apellido=req.body.apellido;
     let carrera=req.body.carrera;
@@ -41,9 +60,9 @@ app.post("/nuevo",(req,res)=>{
     nuevo(connection,{nombre:nombre,apellido:apellido,prepa:prepa,carrera:carrera,fecha:fecha,genero:genero,promedio:promedio},result=>{
         res.send(result);
     });
-})
+})*/
 
-app.post("/eliminar",(req,res)=>{
+/*app.post("/eliminar",(req,res)=>{
     let id=req.body.id;
     eliminar(connection,{id:id},result=>{
         res.send(result);
@@ -55,7 +74,7 @@ app.get("/mostrar",(req,res)=>{
     mostrar(connection,result=>{
         res.send(result);
     })
-})
+})*/
 
 app.listen(puerto, () => {
   console.log(`Servidor corriendo en el puerto `+puerto);
